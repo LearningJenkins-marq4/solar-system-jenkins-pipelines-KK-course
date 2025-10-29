2b4d1abb0a3576450a0738f5337dd5ce4e9b1901
marq4 <elapamarquina@hotmail.com>
2025-10-29 12:51:15 -0600

    Stage: Integration Testing..

--- integration-testing-ec2.sh
+++ integration-testing-ec2.sh
@@ -15,14 +15,14 @@ then
     exit 1
 fi
 
-return_code=$(curl -s -o /dev/null -w "${return_code}" http://${public_dns_name}:3000/live)
+return_code=$(curl -s -o /dev/null -w "%{http_code}" http://${public_dns_name}:3000/live)
 echo "Return code => ${return_code}."
 planet_data=$(curl -s -X POST http://${public_dns_name}:3000/planet -H "Content-Type: application/json" -d '{"id": "3"}')
 echo "Planet data => ${planet_data}."
 planet_name=$(echo ${planet_data} | jq .name -r)
 echo "Planet name => ${planet_name}."
 
-if [[ "${return_code}" -eq 200 && "${planet_name}" -eq 'Earth' ]]
+if [[ "${return_code}" -eq 200 && "${planet_name}" == 'Earth' ]]
 then
     echo "Tests passed!"
 else
